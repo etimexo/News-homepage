fa9bcdd06d9ceaaf81bf8f87592be01fc1cbc61f
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-06-19 23:24:08 +0100

    Edit files

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const toggler = document.querySelector(".toggle")
 const navMenu = document.querySelector(".nav-menu")
+const togglerImage = toggler.querySelector("img");
+const toggleOpenImage = "./assets/images/icon-menu.svg";
+const toggleCloseImage = "./assets/images/icon-menu-close.svg";
 toggler.addEventListener("click", () => {
     toggler.classList.toggle("active")
     navMenu.classList.toggle("active")
+    if (toggler.classList.contains("active")) {
+        togglerImage.src = toggleCloseImage;
+      } else {
+        togglerImage.src = toggleOpenImage;
+      }
 }) 
+
+
 document.addEventListener("click", (event) => {
     const target = event.target
     const isNavMenuClicked = target.classList.contains("nav-menu") || target.closest(".nav-menu")
@@ -11,6 +21,7 @@ document.addEventListener("click", (event) => {
     if (!isNavMenuClicked && !isTogglerClicked) {
       toggler.classList.remove("active")
       navMenu.classList.remove("active")
+      togglerImage.src = toggleOpenImage;
     }
 })
 document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
